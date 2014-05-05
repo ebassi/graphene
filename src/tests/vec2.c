5c9ff95cfd61f79d38e3c97ce58dde7bf2cade53
Emmanuele Bassi <ebassi@gnome.org>
2014-05-05 19:19:27 +0100

    tests/vec2: Test all const vectors

--- src/tests/vec2.c
+++ src/tests/vec2.c
@@ -18,6 +18,18 @@ vectors_vec2_const (void)
   g_assert_true (vec2 != graphene_vec2_zero ());
   g_assert_cmpfloat (graphene_vec2_get_x (vec2), ==, 1.f);
   g_assert_cmpfloat (graphene_vec2_get_y (vec2), ==, 1.f);
+
+  vec2 = graphene_vec2_x_axis ();
+  g_assert_nonnull (vec2);
+  g_assert_true (vec2 == graphene_vec2_x_axis ());
+  g_assert_cmpfloat (graphene_vec2_get_x (vec2), ==, 1.f);
+  g_assert_cmpfloat (graphene_vec2_get_y (vec2), ==, 0.f);
+
+  vec2 = graphene_vec2_y_axis ();
+  g_assert_nonnull (vec2);
+  g_assert_true (vec2 == graphene_vec2_y_axis ());
+  g_assert_cmpfloat (graphene_vec2_get_x (vec2), ==, 0.f);
+  g_assert_cmpfloat (graphene_vec2_get_y (vec2), ==, 1.f);
 }
 
 static void
