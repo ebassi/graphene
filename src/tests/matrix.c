6d658591ceb52cf9297d9becac03012c596fc7a1
Lubosz Sarnecki <lubosz@gmail.com>
2014-06-03 14:19:48 +0200

    tests: add some matrix tests

--- src/tests/matrix.c
+++ src/tests/matrix.c
@@ -2,6 +2,31 @@
 #include <graphene.h>
 
 static void
+print_matrix (graphene_matrix_t * m)
+{
+  int i;
+  g_print("\n");
+  for (i = 0; i < 4; i++) {
+    g_print ("%.3f %.3f %.3f %.3f\n",
+      graphene_matrix_get_value (m, i, 0),
+      graphene_matrix_get_value (m, i, 1),
+      graphene_matrix_get_value (m, i, 2),
+      graphene_matrix_get_value (m, i, 3));
+  }
+}
+
+static void
+compare_matrices (graphene_matrix_t * m1, graphene_matrix_t * m2)
+{
+  int x, y;
+  for (x = 0; x < 4; x++)
+    for (y = 0; y < 4; y++)
+      g_assert_cmpfloat (
+        graphene_matrix_get_value (m1, x, y), ==,
+        graphene_matrix_get_value (m2, x, y));
+}
+
+static void
 matrix_identity (void)
 {
   graphene_matrix_t m;
@@ -9,6 +34,7 @@ matrix_identity (void)
 
   graphene_matrix_init_identity (&m);
   g_assert_true (graphene_matrix_is_identity (&m));
+  print_matrix(&m);
 
   g_assert_cmpfloat (graphene_matrix_get_value (&m, 0, 0), ==, 1.f);
   g_assert_cmpfloat (graphene_matrix_get_value (&m, 1, 1), ==, 1.f);
@@ -24,12 +50,69 @@ matrix_identity (void)
   g_assert_false (graphene_matrix_is_identity (&m));
 }
 
+static void
+matrix_rotation (void)
+{
+  graphene_matrix_t m;
+  graphene_matrix_t m2;
+
+  graphene_vec3_t rot;
+  graphene_vec3_init (&rot, 1.0, 0.0, 0.0);
+
+  graphene_matrix_init_identity (&m);
+  g_assert_true (graphene_matrix_is_identity (&m));
+
+  graphene_matrix_rotate (&m, 30.0f, &rot);
+  graphene_matrix_init_rotate (&m2, 30.0f, &rot);
+  print_matrix(&m);
+
+  compare_matrices(&m, &m2);
+}
+
+static void
+matrix_translation (void)
+{
+  graphene_matrix_t m;
+  graphene_matrix_t m2;
+
+  graphene_point3d_t trans =
+      GRAPHENE_POINT3D_INIT (1.0f, 2.0f, 3.0f);
+
+  graphene_matrix_init_identity (&m);
+  g_assert_true (graphene_matrix_is_identity (&m));
+
+  graphene_matrix_translate (&m, &trans);
+  graphene_matrix_init_translate (&m2, &trans);
+  print_matrix(&m);
+
+  compare_matrices(&m, &m2);
+}
+
+static void
+matrix_scale (void)
+{
+  graphene_matrix_t m;
+  graphene_matrix_t m2;
+
+  graphene_matrix_init_identity (&m);
+  g_assert_true (graphene_matrix_is_identity (&m));
+
+  graphene_matrix_scale (&m, 1.0f, 2.0f, 3.0f);
+  graphene_matrix_init_scale (&m2, 1.0f, 2.0f, 3.0f);
+  print_matrix(&m);
+
+  compare_matrices(&m, &m2);
+}
+
 int
 main (int argc, char *argv[])
 {
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/matrix/identity", matrix_identity);
+  g_test_add_func ("/matrix/scale", matrix_scale);
+  g_test_add_func ("/matrix/rotation", matrix_rotation);
+  g_test_add_func ("/matrix/translation", matrix_translation);
 
   return g_test_run ();
 }
