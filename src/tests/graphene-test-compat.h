a0d86bbe71ac11e48c5732b3dbf2d79b96efba5d
Chun-wei Fan <fanchunwei@src.gnome.org>
2014-10-29 17:07:41 +0800

    tests/graphene-test-compat.h: Allow Build on MSVC
    Define graphene_assert_fuzzy_equals that is suitable on Visual Studio
    builds, as it does not support typeof, and there are some differences in
    syntax.  Later Visual Studio versions support similar functionality with
    decltype.

--- src/tests/graphene-test-compat.h
+++ src/tests/graphene-test-compat.h
@@ -7,6 +7,8 @@
 # define g_assert_nonnull(expr)         g_assert ((expr) != NULL)
 #endif
 
+#if !defined (_MSC_VER)
+/* Use typeof on GCC */
 #define graphene_assert_fuzzy_equals(n1,n2,epsilon) \
   G_STMT_START { \
     typeof ((n1)) __n1 = (n1); \
@@ -26,3 +28,48 @@
       } \
     } \
   } G_STMT_END
+
+/* !_MSC_VER */
+#elif (_MSC_VER >= 1600)
+/* Use decltype on MSVC 2010+ */
+#define graphene_assert_fuzzy_equals(n1,n2,epsilon) \
+  G_STMT_START { \
+    decltype (n1) __n1 = n1; \
+    decltype (n2) __n2 = n2; \
+    decltype (epsilon) __epsilon = epsilon; \
+    if (__n1 > __n2) { \
+      if ((__n1 - __n2) < __epsilon) ; else { \
+        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
+                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
+                                    __n1, "==", __n2, 'f'); \
+      } \
+    } else { \
+      if ((__n2 - __n1) < __epsilon) ; else { \
+        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
+                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
+                                    __n1, "==", __n2, 'f'); \
+      } \
+    } \
+  } G_STMT_END
+
+#else /* _MSC_VER >= 1600 */
+/* fallback for Visual Studio, decltype not supported */
+#define graphene_assert_fuzzy_equals(n1,n2,epsilon) \
+  G_STMT_START { \
+    if (n1 > n2) { \
+      if (n1 - n2 < epsilon) ; else { \
+        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
+                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
+                                    n1, "==", n2, 'f'); \
+      } \
+    } else { \
+      if (n2 - n1 < epsilon) ; else { \
+        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
+                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
+                                    n1, "==", n2, 'f'); \
+      } \
+    } \
+  } G_STMT_END
+
+/* _MSC_VER < 1600 */
+#endif
