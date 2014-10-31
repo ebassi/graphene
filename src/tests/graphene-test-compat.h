aab47cdf85f35722663ce2a103089e3a55688b57
Chun-wei Fan <fanchunwei@src.gnome.org>
2014-10-31 19:29:27 +0800

    graphene-test-compat.h: Remove MSVC 2010+ Code Path
    Apparently decltype is only available for C++, so it won't work for plain
    C programs, so remove the MSVC 2010+ codepath that was trying to use it.

--- src/tests/graphene-test-compat.h
+++ src/tests/graphene-test-compat.h
@@ -30,29 +30,7 @@
   } G_STMT_END
 
 /* !_MSC_VER */
-#elif (_MSC_VER >= 1600)
-/* Use decltype on MSVC 2010+ */
-#define graphene_assert_fuzzy_equals(n1,n2,epsilon) \
-  G_STMT_START { \
-    decltype (n1) __n1 = n1; \
-    decltype (n2) __n2 = n2; \
-    decltype (epsilon) __epsilon = epsilon; \
-    if (__n1 > __n2) { \
-      if ((__n1 - __n2) < __epsilon) ; else { \
-        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
-                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
-                                    __n1, "==", __n2, 'f'); \
-      } \
-    } else { \
-      if ((__n2 - __n1) < __epsilon) ; else { \
-        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
-                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
-                                    __n1, "==", __n2, 'f'); \
-      } \
-    } \
-  } G_STMT_END
-
-#else /* _MSC_VER >= 1600 */
+#else
 /* fallback for Visual Studio, decltype not supported */
 #define graphene_assert_fuzzy_equals(n1,n2,epsilon) \
   G_STMT_START { \
@@ -71,5 +49,5 @@
     } \
   } G_STMT_END
 
-/* _MSC_VER < 1600 */
+/* _MSC_VER */
 #endif
