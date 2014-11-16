d4b527818a5655e03d13db9dd80085112b95aabf
Emmanuele Bassi <ebassi@gnome.org>
2014-11-16 17:18:56 +0000

    tests/plane: Improve coverage
    Add more unit tests for graphene_plane_t.

--- src/tests/plane.c
+++ src/tests/plane.c
@@ -27,7 +27,81 @@ GRAPHENE_TEST_UNIT_BEGIN (plane_init_base)
 }
 GRAPHENE_TEST_UNIT_END
 
+GRAPHENE_TEST_UNIT_BEGIN (plane_init_normal_point)
+{
+  graphene_point3d_t zero = GRAPHENE_POINT3D_INIT_ZERO;
+  graphene_vec3_t normal, check;
+  graphene_plane_t *p;
+
+  graphene_vec3_normalize (graphene_vec3_one (), &normal);
+
+  p = graphene_plane_init_from_point (graphene_plane_alloc (), &normal, &zero);
+
+  graphene_plane_get_normal (p, &check);
+  g_assert_true (graphene_vec3_equal (&check, &normal));
+  g_assert_cmpfloat (graphene_plane_get_constant (p), ==, 0.f);
+
+  graphene_plane_free (p);
+}
+GRAPHENE_TEST_UNIT_END
+
+GRAPHENE_TEST_UNIT_BEGIN (plane_normalize)
+{
+  graphene_plane_t p;
+  graphene_vec3_t tmp;
+
+  graphene_plane_init (&p, graphene_vec3_init (&tmp, 2.f, 0.f, 0.f), 2.f);
+
+  graphene_plane_normalize (&p, &p);
+  graphene_plane_get_normal (&p, &tmp);
+  graphene_assert_fuzzy_equals (graphene_vec3_length (&tmp), 1.f, 0.0001);
+  graphene_assert_fuzzy_equals (graphene_plane_get_constant (&p), 1.f, 0.0001);
+}
+GRAPHENE_TEST_UNIT_END
+
+GRAPHENE_TEST_UNIT_BEGIN (plane_distance_positive)
+{
+  graphene_plane_t p;
+  graphene_vec3_t tmp;
+  graphene_point3d_t point;
+
+  graphene_plane_init (&p, graphene_vec3_init (&tmp, 2.f, 0.f, 0.f), -2.f);
+  graphene_plane_normalize (&p, &p);
+
+  graphene_point3d_init (&point, 4.f, 0.f, 0.f);
+  graphene_assert_fuzzy_equals (graphene_plane_distance (&p, &point), 3.f, 0.0001);
+
+  graphene_point3d_init (&point, 1.f, 0.f, 0.f);
+  graphene_assert_fuzzy_equals (graphene_plane_distance (&p, &point), 0.f, 0.0001);
+
+  graphene_plane_negate (&p, &p);
+
+  graphene_point3d_init (&point, 4.f, 0.f, 0.f);
+  graphene_assert_fuzzy_equals (graphene_plane_distance (&p, &point), -3.f, 0.0001);
+
+  graphene_point3d_init (&point, 1.f, 0.f, 0.f);
+  graphene_assert_fuzzy_equals (graphene_plane_distance (&p, &point), 0.f, 0.0001);
+}
+GRAPHENE_TEST_UNIT_END
+
+GRAPHENE_TEST_UNIT_BEGIN (plane_distance_negative)
+{
+  graphene_plane_t p;
+  graphene_vec3_t tmp;
+  graphene_point3d_t point;
+
+  graphene_plane_init (&p, graphene_vec3_init (&tmp, 2.f, 0.f, 0.f), -2.f);
+  graphene_plane_normalize (&p, &p);
+
+  graphene_point3d_init (&point, 4.f, 0.f, 0.f);
+  graphene_assert_fuzzy_equals (graphene_plane_distance (&p, &point), 3.f, 0.0001);
+}
+GRAPHENE_TEST_UNIT_END
 
 GRAPHENE_TEST_SUITE (
   GRAPHENE_TEST_UNIT ("/plane/init/base", plane_init_base)
+  GRAPHENE_TEST_UNIT ("/plane/init/normal-point", plane_init_normal_point)
+  GRAPHENE_TEST_UNIT ("/plane/normalize", plane_normalize)
+  GRAPHENE_TEST_UNIT ("/plane/distance/positive", plane_distance_positive)
+  GRAPHENE_TEST_UNIT ("/plane/distance/negative", plane_distance_negative)
 )
