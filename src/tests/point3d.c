22907af8269130540429d7a4f1d57174d6e39405
Emmanuele Bassi <ebassi@gnome.org>
2014-11-16 22:35:50 +0000

    tests/point3d: Port to the test suite boilerplate macros

--- src/tests/point3d.c
+++ src/tests/point3d.c
@@ -4,8 +4,7 @@
 
 #include "graphene-test-compat.h"
 
-static void
-point_zero (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_zero)
 {
   const graphene_point3d_t *p;
   graphene_point3d_t q;
@@ -24,24 +23,24 @@ point_zero (void)
 
   g_assert_true (graphene_point3d_equal (p, &q));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_alloc (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_alloc)
 {
   graphene_point3d_t *a, b;
 
   a = graphene_point3d_alloc ();
   g_assert_nonnull (a);
 
-  graphene_point3d_init (a, 10.f, 10.f, 10.f);
+  graphene_point3d_init (a, x, y, z);
   graphene_point3d_init_from_point (&b, a);
   g_assert_true (graphene_point3d_equal (&b, a));
 
   graphene_point3d_free (a);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_init (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_init)
 {
   graphene_point3d_t p, q, r;
   graphene_vec3_t vec3;
@@ -61,21 +60,21 @@ point_init (void)
   g_assert_cmpfloat (q.y, ==, p.y);
   g_assert_cmpfloat (q.z, ==, p.z);
 
-  graphene_vec3_init (&vec3, 1.f, 2.f, 3.f);
+  graphene_vec3_init (&vec3, x, y, w);
   graphene_point3d_init_from_vec3 (&r, &vec3);
   g_assert_cmpfloat (graphene_vec3_get_x (&vec3), ==, r.x);
   g_assert_cmpfloat (graphene_vec3_get_y (&vec3), ==, r.y);
   g_assert_cmpfloat (graphene_vec3_get_z (&vec3), ==, r.z);
 
-  graphene_point3d_init (&r, 4.f, 5.f, 6.f);
+  graphene_point3d_init (&r, x, y, z);
   graphene_point3d_to_vec3 (&r, &vec3);
-  g_assert_cmpfloat (graphene_vec3_get_x (&vec3), ==, 4.f);
-  g_assert_cmpfloat (graphene_vec3_get_y (&vec3), ==, 5.f);
-  g_assert_cmpfloat (graphene_vec3_get_z (&vec3), ==, 6.f);
+  g_assert_cmpfloat (graphene_vec3_get_x (&vec3), ==, x);
+  g_assert_cmpfloat (graphene_vec3_get_y (&vec3), ==, y);
+  g_assert_cmpfloat (graphene_vec3_get_z (&vec3), ==, z);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_equal (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_equal)
 {
   graphene_point3d_t p, q;
 
@@ -90,9 +89,9 @@ point_equal (void)
   graphene_point3d_init (&q, 1.f, 1.f, 1.f);
   g_assert_false (graphene_point3d_equal (&p, &q));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_near (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_near)
 {
   graphene_point3d_t p, q;
 
@@ -104,9 +103,9 @@ point_near (void)
   g_assert_false (graphene_point3d_near (&p, &q, 0.f));
   g_assert_true (graphene_point3d_near (&p, &p, 0.f));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_interpolate (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_interpolate)
 {
   graphene_point3d_t p, half, q, res;
 
@@ -129,27 +128,27 @@ point_interpolate (void)
   g_assert_cmpfloat (res.y, ==, q.y);
   g_assert_cmpfloat (res.z, ==, q.z);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_scale (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_scale)
 {
   graphene_point3d_t p;
 
-  graphene_point3d_init (&p, 1.f, 2.f, 3.f);
+  graphene_point3d_init (&p, x, y, z);
   graphene_point3d_scale (&p, 2.f, &p);
-  g_assert_cmpfloat (p.x, ==, 2.f);
-  g_assert_cmpfloat (p.y, ==, 4.f);
-  g_assert_cmpfloat (p.z, ==, 6.f);
+  g_assert_cmpfloat (p.x, ==, x * 2.f);
+  g_assert_cmpfloat (p.y, ==, y * 2.f);
+  g_assert_cmpfloat (p.z, ==, z * 2.f);
 
-  graphene_point3d_init (&p, 1.f, 2.f, 4.f);
+  graphene_point3d_init (&p, x, y, z);
   graphene_point3d_scale (&p, .5f, &p);
-  g_assert_cmpfloat (p.x, ==, .5f);
-  g_assert_cmpfloat (p.y, ==, 1.f);
-  g_assert_cmpfloat (p.z, ==, 2.f);
+  g_assert_cmpfloat (p.x, ==, x * .5f);
+  g_assert_cmpfloat (p.y, ==, y * .5f);
+  g_assert_cmpfloat (p.z, ==, z * .5f);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_cross (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_cross)
 {
   graphene_point3d_t a, b, res;
   graphene_vec3_t v_a, v_b, v_res;
@@ -167,9 +166,9 @@ point_cross (void)
   g_assert_cmpfloat (res.y, ==, graphene_vec3_get_y (&v_res));
   g_assert_cmpfloat (res.z, ==, graphene_vec3_get_z (&v_res));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_dot (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_dot)
 {
   graphene_point3d_t a, b;
   graphene_vec3_t v_a, v_b;
@@ -182,9 +181,9 @@ point_dot (void)
 
   g_assert_cmpfloat (graphene_point3d_dot (&a, &b), ==, graphene_vec3_dot (&v_a, &v_b));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_length (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_length)
 {
   graphene_point3d_t p;
   float res;
@@ -194,9 +193,9 @@ point_length (void)
 
   g_assert_cmpfloat (graphene_point3d_length (&p), ==, res);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_normalize (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_normalize)
 {
   graphene_point3d_t p, q;
 
@@ -205,23 +204,18 @@ point_normalize (void)
 
   g_assert_false (graphene_point3d_equal (&p, &q));
 }
+GRAPHENE_TEST_UNIT_END
 
-int
-main (int argc, char *argv[])
-{
-  g_test_init (&argc, &argv, NULL);
-
-  g_test_add_func ("/point3d/alloc", point_alloc);
-  g_test_add_func ("/point3d/init", point_init);
-  g_test_add_func ("/point3d/equal", point_equal);
-  g_test_add_func ("/point3d/near", point_near);
-  g_test_add_func ("/point3d/zero", point_zero);
-  g_test_add_func ("/point3d/interpolate", point_interpolate);
-  g_test_add_func ("/point3d/scale", point_scale);
-  g_test_add_func ("/point3d/cross", point_cross);
-  g_test_add_func ("/point3d/dot", point_dot);
-  g_test_add_func ("/point3d/length", point_length);
-  g_test_add_func ("/point3d/normalize", point_normalize);
-
-  return g_test_run ();
-}
+GRAPHENE_TEST_SUITE (
+  GRAPHENE_TEST_UNIT ("/point3d/alloc", point_alloc)
+  GRAPHENE_TEST_UNIT ("/point3d/init", point_init)
+  GRAPHENE_TEST_UNIT ("/point3d/equal", point_equal)
+  GRAPHENE_TEST_UNIT ("/point3d/near", point_near)
+  GRAPHENE_TEST_UNIT ("/point3d/zero", point_zero)
+  GRAPHENE_TEST_UNIT ("/point3d/interpolate", point_interpolate)
+  GRAPHENE_TEST_UNIT ("/point3d/scale", point_scale)
+  GRAPHENE_TEST_UNIT ("/point3d/cross", point_cross)
+  GRAPHENE_TEST_UNIT ("/point3d/dot", point_dot)
+  GRAPHENE_TEST_UNIT ("/point3d/length", point_length)
+  GRAPHENE_TEST_UNIT ("/point3d/normalize", point_normalize)
+)
