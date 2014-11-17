ab7aa35d5e99a1dbfee5a9180ed599dc79701f6d
Emmanuele Bassi <ebassi@gnome.org>
2014-11-17 14:57:49 +0000

    tests/triangle: Port to the boilerplate macros

--- src/tests/triangle.c
+++ src/tests/triangle.c
@@ -3,8 +3,7 @@
 
 #include "graphene-test-compat.h"
 
-static void
-triangle_init_from_point (void)
+GRAPHENE_TEST_UNIT_BEGIN (triangle_init_from_point)
 {
   graphene_point3d_t a = GRAPHENE_POINT3D_INIT ( 0.f,  1.f, 0.f);
   graphene_point3d_t b = GRAPHENE_POINT3D_INIT ( 1.f, -1.f, 0.f);
@@ -15,13 +14,8 @@ triangle_init_from_point (void)
 
   g_assert_cmpfloat (graphene_triangle_get_area (&t), ==, 2.f);
 }
+GRAPHENE_TEST_UNIT_END
 
-int
-main (int argc, char *argv[])
-{
-  g_test_init (&argc, &argv, NULL);
-
-  g_test_add_func ("/triangle/init/from-point", triangle_init_from_point);
-
-  return g_test_run ();
-}
+GRAPHENE_TEST_SUITE (
+  GRAPHENE_TEST_UNIT ("/triangle/init/from-point", triangle_init_from_point)
+)
