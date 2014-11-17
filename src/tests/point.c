7ded5c6f966c52d7a519e595fe3fc32b8c110d6a
Emmanuele Bassi <ebassi@gnome.org>
2014-11-17 11:54:58 +0000

    tests/point: Port to the boilerplate macros

--- src/tests/point.c
+++ src/tests/point.c
@@ -4,8 +4,7 @@
 
 #include "graphene-test-compat.h"
 
-static void
-point_zero (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_zero)
 {
   const graphene_point_t *p;
   graphene_point_t q;
@@ -13,32 +12,30 @@ point_zero (void)
   p = graphene_point_zero ();
   g_assert_nonnull (p);
   g_assert_true (p == graphene_point_zero ());
-  g_assert_cmpfloat (p->x, ==, 0.f);
-  g_assert_cmpfloat (p->y, ==, 0.f);
+  g_assert_true (graphene_point_equal (p, &zero2));
 
   graphene_point_init_from_point (&q, graphene_point_zero ());
-  g_assert_cmpfloat (q.x, ==, 0.f);
-  g_assert_cmpfloat (q.y, ==, 0.f);
+  g_assert_true (graphene_point_equal (&q, &zero2));
   g_assert_cmpfloat (graphene_point_distance (p, &q, NULL, NULL), ==, 0.f);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_alloc (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_alloc)
 {
   graphene_point_t *a, b;
 
   a = graphene_point_alloc ();
   g_assert_nonnull (a);
 
-  graphene_point_init (a, 10.f, 10.f);
+  graphene_point_init (a, x, y);
   graphene_point_init_from_point (&b, a);
   g_assert_true (graphene_point_equal (&b, a));
 
   graphene_point_free (a);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_init (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_init)
 {
   graphene_point_t p, q;
 
@@ -54,9 +51,9 @@ point_init (void)
   g_assert_cmpfloat (q.x, ==, p.x);
   g_assert_cmpfloat (q.y, ==, p.y);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_equal (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_equal)
 {
   graphene_point_t p, q;
 
@@ -71,9 +68,9 @@ point_equal (void)
   graphene_point_init (&q, 1.f, 1.f);
   g_assert_false (graphene_point_equal (&p, &q));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_distance (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_distance)
 {
   graphene_point_t p, q;
   float x_d, y_d;
@@ -96,9 +93,9 @@ point_distance (void)
   g_assert_cmpfloat (x_d, ==, 1.f);
   g_assert_cmpfloat (y_d, ==, 1.f);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_near (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_near)
 {
   graphene_point_t p, q;
 
@@ -110,9 +107,9 @@ point_near (void)
   g_assert_false (graphene_point_near (&p, &q, 0.f));
   g_assert_true (graphene_point_near (&p, &p, 0.f));
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-point_interpolate (void)
+GRAPHENE_TEST_UNIT_BEGIN (point_interpolate)
 {
   graphene_point_t p, half, q, res;
 
@@ -132,19 +129,14 @@ point_interpolate (void)
   g_assert_cmpfloat (res.x, ==, q.x);
   g_assert_cmpfloat (res.y, ==, q.y);
 }
+GRAPHENE_TEST_UNIT_END
 
-int
-main (int argc, char *argv[])
-{
-  g_test_init (&argc, &argv, NULL);
-
-  g_test_add_func ("/point/alloc", point_alloc);
-  g_test_add_func ("/point/init", point_init);
-  g_test_add_func ("/point/equal", point_equal);
-  g_test_add_func ("/point/distance", point_distance);
-  g_test_add_func ("/point/near", point_near);
-  g_test_add_func ("/point/zero", point_zero);
-  g_test_add_func ("/point/interpolate", point_interpolate);
-
-  return g_test_run ();
-}
+GRAPHENE_TEST_SUITE (
+  GRAPHENE_TEST_UNIT ("/point/alloc", point_alloc)
+  GRAPHENE_TEST_UNIT ("/point/init", point_init)
+  GRAPHENE_TEST_UNIT ("/point/equal", point_equal)
+  GRAPHENE_TEST_UNIT ("/point/distance", point_distance)
+  GRAPHENE_TEST_UNIT ("/point/near", point_near)
+  GRAPHENE_TEST_UNIT ("/point/zero", point_zero)
+  GRAPHENE_TEST_UNIT ("/point/interpolate", point_interpolate)
+)
