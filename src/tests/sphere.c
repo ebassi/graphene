8a5c9fd809083abce2c759edbb5003fec45cfa2e
Emmanuele Bassi <ebassi@gnome.org>
2014-11-17 14:57:34 +0000

    tests/sphere: Port to the boilerplate macros

--- src/tests/sphere.c
+++ src/tests/sphere.c
@@ -3,41 +3,37 @@
 
 #include "graphene-test-compat.h"
 
-static void
-sphere_init (void)
+GRAPHENE_TEST_UNIT_BEGIN (sphere_init)
 {
-  graphene_point3d_t center = GRAPHENE_POINT3D_INIT (0.f, 0.f, 0.f);
   graphene_point3d_t check;
   graphene_sphere_t *s;
 
-  s = graphene_sphere_init (graphene_sphere_alloc (), &center, 1.f);
+  s = graphene_sphere_init (graphene_sphere_alloc (), &zero3, 1.f);
   g_assert_nonnull (s);
   g_assert_false (graphene_sphere_is_empty (s));
 
   graphene_sphere_get_center (s, &check);
-  g_assert_true (graphene_point3d_equal (&center, &check));
+  g_assert_true (graphene_point3d_equal (&zero3, &check));
   g_assert_cmpfloat (graphene_sphere_get_radius (s), ==, 1.f);
 
   graphene_sphere_init (s, NULL, 1.f);
   graphene_sphere_get_center (s, &check);
-  g_assert_true (graphene_point3d_equal (&check, &check));
+  g_assert_true (graphene_point3d_equal (&zero3, &check));
   g_assert_cmpfloat (graphene_sphere_get_radius (s), ==, 1.f);
 
   graphene_sphere_free (s);
 }
+GRAPHENE_TEST_UNIT_END
 
-static void
-sphere_contains_point (void)
+GRAPHENE_TEST_UNIT_BEGIN (sphere_contains_point)
 {
-  graphene_point3d_t center = GRAPHENE_POINT3D_INIT (0.f, 0.f, 0.f);
   graphene_point3d_t check;
   graphene_sphere_t *s;
 
-  s = graphene_sphere_init (graphene_sphere_alloc (), &center, 1.f);
+  s = graphene_sphere_init (graphene_sphere_alloc (), &zero3, 1.f);
   g_assert_nonnull (s);
 
-  graphene_point3d_init (&check, 0.f, 0.f, 0.f);
-  g_assert_true (graphene_sphere_contains_point (s, &check));
+  g_assert_true (graphene_sphere_contains_point (s, &zero3));
 
   graphene_point3d_init (&check, 1.f, 0.f, 0.f);
   g_assert_true (graphene_sphere_contains_point (s, &check));
@@ -45,19 +41,13 @@ sphere_contains_point (void)
   graphene_point3d_init (&check, 0.f, -1.f, 0.f);
   g_assert_true (graphene_sphere_contains_point (s, &check));
 
-  graphene_point3d_init (&check, 1.f, 1.f, 1.f);
-  g_assert_false (graphene_sphere_contains_point (s, &check));
+  g_assert_false (graphene_sphere_contains_point (s, &one3));
 
   graphene_sphere_free (s);
 }
+GRAPHENE_TEST_UNIT_END
 
-int
-main (int argc, char *argv[])
-{
-  g_test_init (&argc, &argv, NULL);
-
-  g_test_add_func ("/sphere/init", sphere_init);
-  g_test_add_func ("/sphere/contains-point", sphere_contains_point);
-
-  return g_test_run ();
-}
+GRAPHENE_TEST_SUITE (
+  GRAPHENE_TEST_UNIT ("/sphere/init", sphere_init)
+  GRAPHENE_TEST_UNIT ("/sphere/contains-point", sphere_contains_point)
+)
