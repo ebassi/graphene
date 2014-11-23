17da2bc8a6884c9b078924a3f4f21854a9ce5dce
Emmanuele Bassi <ebassi@gnome.org>
2014-11-23 12:02:11 +0000

    tests/euler: Check roundtrips between Euler and Quaternion

--- src/tests/euler.c
+++ src/tests/euler.c
@@ -16,6 +16,31 @@ GRAPHENE_TEST_UNIT_BEGIN (euler_init)
 }
 GRAPHENE_TEST_UNIT_END
 
+GRAPHENE_TEST_UNIT_BEGIN (euler_quaternion_roundtrip)
+{
+  graphene_euler_t values[3];
+  unsigned int i;
+
+  graphene_euler_init_with_order (&values[0], 0.f, 0.f, 0.f, GRAPHENE_EULER_ORDER_XYZ);
+  graphene_euler_init_with_order (&values[1], 1.f, 0.f, 0.f, GRAPHENE_EULER_ORDER_XYZ);
+  graphene_euler_init_with_order (&values[2], 0.f, 1.f, 0.f, GRAPHENE_EULER_ORDER_ZYX);
+
+  for (i = 0; i < G_N_ELEMENTS (values); i++)
+    {
+      graphene_quaternion_t q, check;
+      graphene_euler_t e;
+
+      graphene_quaternion_init_from_euler (&q, &values[i]);
+
+      graphene_euler_init_from_quaternion (&e, &q, graphene_euler_get_order (&values[i]));
+      graphene_quaternion_init_from_euler (&check, &e);
+
+      g_assert_true (graphene_quaternion_equal (&q, &check));
+    }
+}
+GRAPHENE_TEST_UNIT_END
+
 GRAPHENE_TEST_SUITE (
   GRAPHENE_TEST_UNIT ("/euler/init", euler_init)
+  GRAPHENE_TEST_UNIT ("/euler/quaternion-roundtrip", euler_quaternion_roundtrip)
 )
