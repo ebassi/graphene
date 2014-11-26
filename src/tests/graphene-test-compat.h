b3065ba3d7587dd87044bfdd35d11c4ccf72ec8b
Emmanuele Bassi <ebassi@gnome.org>
2014-11-26 13:21:03 +0000

    tests: Rework the fuzzy comparison macros
    Simplify the condition checking, and add a specialized fuzzy comparison
    macro for matrices that takes the row and column being compared.

--- src/tests/graphene-test-compat.h
+++ src/tests/graphene-test-compat.h
@@ -14,18 +14,23 @@
     typeof ((n1)) __n1 = (n1); \
     typeof ((n2)) __n2 = (n2); \
     typeof ((epsilon)) __epsilon = (epsilon); \
-    if (__n1 > __n2) { \
-      if ((__n1 - __n2) < __epsilon) ; else { \
-        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
-                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
-                                    __n1, "==", __n2, 'f'); \
-      } \
-    } else { \
-      if ((__n2 - __n1) < __epsilon) ; else { \
-        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
-                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
-                                    __n1, "==", __n2, 'f'); \
-      } \
+    if ((__n1 > __n2 ? (__n1 - __n2) : (__n2 - __n1)) < __epsilon) ; else { \
+      g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
+                                  #n1 " == " #n2 " (+/- " #epsilon ")", \
+                                  __n1, "==", __n2, 'f'); \
+    } \
+  } G_STMT_END
+
+# define graphene_assert_fuzzy_matrix_equal(row,col,n1,n2,epsilon) \
+  G_STMT_START { \
+    typeof ((n1)) __n1 = (n1); \
+    typeof ((n2)) __n2 = (n2); \
+    typeof ((epsilon)) __epsilon = (epsilon); \
+    if ((__n1 > __n2 ? (__n1 - __n2) : (__n2 - __n1)) < __epsilon) ; else { \
+      char *s = g_strdup_printf ("[%d][%d]: " #n1 " == " #n2 " (+/- " #epsilon "): (%.7g == %.7g)", \
+                                 row, col, __n1, __n2); \
+      g_assertion_message (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, s); \
+      g_free (s); \
     } \
   } G_STMT_END
 
@@ -34,18 +39,20 @@
 /* fallback for Visual Studio, decltype not supported */
 # define graphene_assert_fuzzy_equals(n1,n2,epsilon) \
   G_STMT_START { \
-    if ((n1) > (n2)) { \
-      if ((n1) - (n2) < epsilon) ; else { \
-        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
-                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
-                                    n1, "==", n2, 'f'); \
-      } \
-    } else { \
-      if ((n2) - (n1) < epsilon) ; else { \
-        g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
-                                    #n1 " == " #n2 " (+/- " #epsilon ")", \
-                                    n1, "==", n2, 'f'); \
-      } \
+    if (((n1) > (n2) ? ((n1) - (n2)) : ((n2) - (n1))) < (epsilon)) ; else { \
+      g_assertion_message_cmpnum (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
+                                  #n1 " == " #n2 " (+/- " #epsilon ")", \
+                                  n1, "==", n2, 'f'); \
+    } \
+  } G_STMT_END
+
+# define graphene_assert_fuzzy_matrix_equal(row,col,n1,n2,epsilon) \
+  G_STMT_START { \
+    if (((n1) > (n2) ? ((n1) - (n2)) : ((n2) - (n1))) < (epsilon)) ; else { \
+      char *s = g_strdup_printf ("[%d][%d]: " #n1 " == " #n2 " (+/- " #epsilon "): (%.7g == %.7g)", \
+                                 row, col, (n1), (n2)); \
+      g_assertion_message (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, s); \
+      g_free (s); \
     } \
   } G_STMT_END
 
