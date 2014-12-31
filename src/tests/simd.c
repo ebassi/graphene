484a551c729bd08d998e868c968e0b666b973113
Emmanuele Bassi <ebassi@gnome.org>
2014-12-31 13:06:43 +0000

    tests/simd: Check the dot() operator

--- src/tests/simd.c
+++ src/tests/simd.c
@@ -95,6 +95,48 @@ simd_compare_gt (void)
   g_assert_false (graphene_simd4f_cmp_gt (c, a));
 }
 
+static void
+simd_operators_dot_2 (void)
+{
+  graphene_simd4f_t a, b, c;
+
+  a = graphene_simd4f_init (1.f, 2.f, 0.f, 0.f);
+  b = graphene_simd4f_init (3.f, 4.f, 0.f, 0.f);
+  c = graphene_simd4f_dot2 (a, b);
+
+  graphene_assert_fuzzy_equals (graphene_simd4f_get_x (c),
+                                3.f * 1.f + 2.f * 4.f,
+                                0.0001f);
+}
+
+static void
+simd_operators_dot_3 (void)
+{
+  graphene_simd4f_t a, b, c;
+
+  a = graphene_simd4f_init (1.f, 2.f, 3.f, 0.f);
+  b = graphene_simd4f_init (4.f, 5.f, 6.f, 0.f);
+  c = graphene_simd4f_dot3 (a, b);
+
+  graphene_assert_fuzzy_equals (graphene_simd4f_get_x (c),
+                                1.f * 4.f + 2.f * 5.f + 3.f * 6.f,
+                                0.0001f);
+}
+
+static void
+simd_operators_dot_4 (void)
+{
+  graphene_simd4f_t a, b, c;
+
+  a = graphene_simd4f_init (1.f, 2.f, 3.f, 4.f);
+  b = graphene_simd4f_init (5.f, 6.f, 7.f, 8.f);
+  c = graphene_simd4f_dot4 (a, b);
+
+  graphene_assert_fuzzy_equals (graphene_simd4f_get_x (c),
+                                1.f * 5.f + 2.f * 6.f + 3.f * 7.f + 4.f * 8.f,
+                                0.0001f);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -106,6 +148,10 @@ main (int argc, char *argv[])
   g_test_add_func ("/simd/compare/le", simd_compare_le);
   g_test_add_func ("/simd/compare/ge", simd_compare_ge);
   g_test_add_func ("/simd/compare/gt", simd_compare_gt);
+
+  g_test_add_func ("/simd/operators/dot/2", simd_operators_dot_2);
+  g_test_add_func ("/simd/operators/dot/3", simd_operators_dot_3);
+  g_test_add_func ("/simd/operators/dot/4", simd_operators_dot_4);
 
   return g_test_run ();
 }
