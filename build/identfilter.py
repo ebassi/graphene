ce326efc310ee056a153df4bf1555f4dbc025229
Emmanuele Bassi <ebassi@gnome.org>
2015-09-09 11:47:27 +0100

    build: Use an external script for filtering type names
    When building introspection data we need to turn our lowercase type
    names ending with '_t' into camel-case identifiers that match the
    expectations of GObject-Introspection.
    
    We currently use a Sed invocation, but we're also relying on GNU Sed
    extensions that are not portable to other operating systems — e.g.
    MacOS and *BSD.
    
    Instead of using Sed, we can use a small Python script that does the
    same transformation, with a higher chance at being portable.
    
    Fixes #42

---
+++ build/identfilter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+NUMBER_REGEX = re.compile(r'([0-9])([a-z])')
+
+def to_camel_case(text):
+	# We only care about types that end with '_t'
+    if not text.endswith('_t'):
+		return text
+
+    res = []
+    for token in text[:-2].split('_'):
+        uc_token = token.title()
+
+		# We need to do this for types like graphene_point3d_t, which
+		# need to be transformed into GraphenePoint3D, not GraphenePoint3d
+        matches = NUMBER_REGEX.match(uc_token)
+        if matches and matches.group(2):
+            uc_token = ''.join([matches.group(1), matches.group(2).title])
+
+        res.append(uc_token)
+
+    return ''.join(res)
+
+if __name__ == '__main__':
+	in_text = sys.stdin.read()
+	sys.stdout.write(to_camel_case(in_text))
