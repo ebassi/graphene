9f812c6f3c0c78272fb20ca3736020d18136f59c
Emmanuele Bassi <ebassi@gnome.org>
2015-09-22 18:08:37 +0100

    point3d: Reintroduce the optimized read
    Instead of the per-channel read, use a vectorised partial read from the
    SIMD into the point3d structure.
    
    This has been confirmed to work on Clang through the test suite.

--- src/graphene-point3d.c
+++ src/graphene-point3d.c
@@ -102,9 +102,7 @@ static inline graphene_point3d_t *
 graphene_point3d_init_from_simd4f (graphene_point3d_t      *p,
                                    const graphene_simd4f_t  v)
 {
-  p->x = graphene_simd4f_get_x (v);
-  p->y = graphene_simd4f_get_y (v);
-  p->z = graphene_simd4f_get_z (v);
+  graphene_simd4f_dup_3f (v, &(p->x));
 
   return p;
 }
