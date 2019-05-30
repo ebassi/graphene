00f3267039a1beab547a15cdfeafef3f6879f4eb
Emmanuele Bassi <ebassi@gnome.org>
2019-05-30 13:06:10 +0100

    Update the MSYS2 build script
     - Install python3 and pip3
     - Install Meson using the same version that we require, instead
       of the latest version from MSYS2
     - Enable debug builds

--- .appveyor/msys2.sh
+++ .appveyor/msys2.sh
@@ -14,14 +14,19 @@ pacman --noconfirm -Suy
 # Install the required packages
 pacman --noconfirm -S --needed \
     base-devel \
-    mingw-w64-$MSYS2_ARCH-toolchain \
+    mingw-w64-$MSYS2_ARCH-gobject-introspection
     mingw-w64-$MSYS2_ARCH-meson \
     mingw-w64-$MSYS2_ARCH-ninja \
     mingw-w64-$MSYS2_ARCH-pkg-config \
-    mingw-w64-$MSYS2_ARCH-gobject-introspection
+    mingw-w64-$MSYS2_ARCH-python3 \
+    mingw-w64-$MSYS2_ARCH-python3-pip \
+    mingw-w64-$MSYS2_ARCH-toolchain \
+
+pip3 install --upgrade --user meson==0.50.1
+export PATH="$HOME/.local/bin:$PATH"
 
 # Build
-meson _build
+meson --buildtype=debug _build
 cd _build
 ninja
 
