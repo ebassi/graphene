453591be1db565369940dda79d6d0b525732c092
Christoph Reiter <reiter.christoph@gmail.com>
2019-06-11 14:25:19 +0200

    CI: work around msys2 toolchain upgrade issues on appveyor
    error: failed to prepare transaction (could not satisfy dependencies)
    :: installing mingw-w64-i686-gcc (9.1.0-1) breaks dependency 'mingw-w64-i686-gcc=7.3.0-2' required by mingw-w64-i686-gcc-ada
    :: installing mingw-w64-i686-gcc (9.1.0-1) breaks dependency 'mingw-w64-i686-gcc=7.3.0-2' required by mingw-w64-i686-gcc-objc
    :: installing mingw-w64-x86_64-gcc (9.1.0-1) breaks dependency 'mingw-w64-x86_64-gcc=8.2.0-3' required by mingw-w64-x86_64-gcc-ada
    :: installing mingw-w64-x86_64-gcc (9.1.0-1) breaks dependency 'mingw-w64-x86_64-gcc=8.2.0-3' required by mingw-w64-x86_64-gcc-objc

--- .appveyor/msys2-pre.sh
+++ .appveyor/msys2-pre.sh
@@ -5,5 +5,9 @@ set -e
 # Disable slow diskspace check
 sed -i 's/^CheckSpace/#CheckSpace/g' /etc/pacman.conf
 
+# Workaround gcc9 update issues
+pacman -R --noconfirm mingw-w64-i686-gcc-ada mingw-w64-i686-gcc-objc || true;
+pacman -R --noconfirm mingw-w64-x86_64-gcc-ada mingw-w64-x86_64-gcc-objc || true;
+
 # System upgrade
 pacman --noconfirm -Syyuu
