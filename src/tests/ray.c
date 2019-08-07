568ce12f852b9f6ebd1e27b5ebdb1e50df996bab
Emmanuele Bassi <ebassi@gnome.org>
2019-08-07 20:19:18 +0100

    tests/ray: Use better descriptions
    The specs and expectations could use better wording.

--- src/tests/ray.c
+++ src/tests/ray.c
@@ -37,19 +37,19 @@ ray_get_position_at (void)
   graphene_ray_init (&r, &one, graphene_vec3_z_axis ());
 
   graphene_ray_get_position_at (&r, 0.f, &tmp);
-  mutest_expect ("querying the origin to return the origin",
+  mutest_expect ("querying a distance of 0 to return the origin",
                  mutest_bool_value (graphene_point3d_equal (&tmp, &one)),
                  mutest_to_be_true,
                  NULL);
 
   graphene_ray_get_position_at (&r, -1.f, &tmp);
-  mutest_expect ("querying a point behind the origin to work",
+  mutest_expect ("querying a negative distance to return a point behind the origin",
                  mutest_bool_value (graphene_point3d_near (&tmp, &GRAPHENE_POINT3D_INIT (1.f, 1.f, 0.f), 0.0001f)),
                  mutest_to_be_true,
                  NULL);
 
   graphene_ray_get_position_at (&r, 1.f, &tmp);
-  mutest_expect ("querying a point on the ray to work",
+  mutest_expect ("querying a positive distance to return a point on the ray",
                  mutest_bool_value (graphene_point3d_near (&tmp, &GRAPHENE_POINT3D_INIT (1.f, 1.f, 2.f), 0.0001f)),
                  mutest_to_be_true,
                  NULL);
@@ -62,17 +62,17 @@ ray_get_distance_to_point (void)
 
   graphene_ray_init (&r, &GRAPHENE_POINT3D_INIT (1, 1, 1), graphene_vec3_z_axis ());
 
-  mutest_expect ("distance on the origin to be zero",
-                 mutest_float_value (graphene_ray_get_distance_to_point (&r, &GRAPHENE_POINT3D_INIT (1, 1, 1))),
-                 mutest_to_be_close_to, 0.0, 0.0001,
-                 NULL);
-
-  mutest_expect ("distance to a point behind the ray to be sqrt(3)",
+  mutest_expect ("closest approach from (0, 0, 0) to be sqrt(3)",
                  mutest_float_value (graphene_ray_get_distance_to_point (&r, &GRAPHENE_POINT3D_INIT (0, 0, 0))),
                  mutest_to_be_close_to, sqrtf (3.f), 0.0001,
                  NULL);
 
-  mutest_expect ("distance to a point in front of the ray to be sqrt(2)",
+  mutest_expect ("closest approach on the origin of the ray to be zero",
+                 mutest_float_value (graphene_ray_get_distance_to_point (&r, &GRAPHENE_POINT3D_INIT (1, 1, 1))),
+                 mutest_to_be_close_to, 0.0, 0.0001,
+                 NULL);
+
+  mutest_expect ("closest approach from (0, 0, 50) to be sqrt(2)",
                  mutest_float_value (graphene_ray_get_distance_to_point (&r, &GRAPHENE_POINT3D_INIT (0, 0, 50))),
                  mutest_to_be_close_to, sqrtf (2.f), 0.0001,
                  NULL);
@@ -131,10 +131,10 @@ ray_matrix_transform (void)
 static void
 ray_suite (void)
 {
-  mutest_it ("initialize", ray_init);
-  mutest_it ("gets position", ray_get_position_at);
-  mutest_it ("gets distance to point", ray_get_distance_to_point);
-  mutest_it ("gets closest point to point", ray_closest_point_to_point);
+  mutest_it ("can be initialized", ray_init);
+  mutest_it ("can compute the position of a point on the ray", ray_get_position_at);
+  mutest_it ("can compute the closest approach of a point from the ray", ray_get_distance_to_point);
+  mutest_it ("can compute the closest point to a point on the ray", ray_closest_point_to_point);
   mutest_it ("can be transformed", ray_matrix_transform);
 }
 
