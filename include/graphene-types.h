36eee28aa7121df3dddd45e9c1d4a322d87bc9e6
Emmanuele Bassi <ebassi@gnome.org>
2019-08-08 16:14:52 +0100

    docs: Give an example for GRAPHENE_VEC[234]_LEN
    It's useful to show how these symbols are meant to be used.

--- include/graphene-types.h
+++ include/graphene-types.h
@@ -40,6 +40,20 @@ GRAPHENE_BEGIN_DECLS
  *
  * Evaluates to the number of components of a #graphene_vec2_t.
  *
+ * This symbol is useful when declaring a C array of floating
+ * point values to be used with graphene_vec2_init_from_float() and
+ * graphene_vec2_to_float(), e.g.
+ *
+ * |[
+ *   float v[GRAPHENE_VEC2_LEN];
+ *
+ *   // vec is defined elsewhere
+ *   graphene_vec2_to_float (&vec, v);
+ *
+ *   for (int i = 0; i < GRAPHENE_VEC2_LEN; i++)
+ *     fprintf (stdout, "component %d: %g\n", i, v[i]);
+ * ]|
+ *
  * Since: 1.0
  */
 #define GRAPHENE_VEC2_LEN       2
@@ -49,6 +63,20 @@ GRAPHENE_BEGIN_DECLS
  *
  * Evaluates to the number of components of a #graphene_vec3_t.
  *
+ * This symbol is useful when declaring a C array of floating
+ * point values to be used with graphene_vec3_init_from_float() and
+ * graphene_vec3_to_float(), e.g.
+ *
+ * |[
+ *   float v[GRAPHENE_VEC3_LEN];
+ *
+ *   // vec is defined elsewhere
+ *   graphene_vec3_to_float (&vec, v);
+ *
+ *   for (int i = 0; i < GRAPHENE_VEC2_LEN; i++)
+ *     fprintf (stdout, "component %d: %g\n", i, v[i]);
+ * ]|
+ *
  * Since: 1.0
  */
 #define GRAPHENE_VEC3_LEN       3
@@ -57,6 +85,20 @@ GRAPHENE_BEGIN_DECLS
  * GRAPHENE_VEC4_LEN:
  *
  * Evaluates to the number of components of a #graphene_vec4_t.
+ *
+ * This symbol is useful when declaring a C array of floating
+ * point values to be used with graphene_vec4_init_from_float() and
+ * graphene_vec4_to_float(), e.g.
+ *
+ * |[
+ *   float v[GRAPHENE_VEC4_LEN];
+ *
+ *   // vec is defined elsewhere
+ *   graphene_vec4_to_float (&vec, v);
+ *
+ *   for (int i = 0; i < GRAPHENE_VEC4_LEN; i++)
+ *     fprintf (stdout, "component %d: %g\n", i, v[i]);
+ * ]|
  *
  * Since: 1.0
  */
