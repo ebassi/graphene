c25e7f15dc8e082af3fe79b235d67dc8a64237ff
Emmanuele Bassi <ebassi@gnome.org>
2024-08-12 15:38:27 +0100

    Fix the GType registration for graphene_box2d_t

--- include/graphene-gobject.h
+++ include/graphene-gobject.h
@@ -149,7 +149,7 @@ GType graphene_ray_get_type (void);
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(graphene_ray_t, graphene_ray_free)
 
-#define GRAPHENE_TYPE_BOX               (graphene_box_get_type ())
+#define GRAPHENE_TYPE_BOX2D             (graphene_box2d_get_type ())
 
 GRAPHENE_AVAILABLE_IN_1_12
 GType graphene_box2d_get_type (void);
