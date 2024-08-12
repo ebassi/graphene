f3af6793ab257f8b1935f74fc3d3bca4c9fb2452
Emmanuele Bassi <ebassi@gnome.org>
2024-08-12 14:09:44 +0100

    build: Rework the public symbol macros and flags
    Use a similar approach as GLib, with an export/import pair of macros
    determined at inclusion time, instead of generating the extern marker at
    build configuration time.
    
    We also add a way to specify a static build with no markers, and we use
    Meson's support for GCC's symbol visibility instead of rolling our own.

--- include/graphene-version-macros.h
+++ include/graphene-version-macros.h
@@ -31,6 +31,25 @@
 
 #include "graphene-version.h"
 
+#if (defined(_WIN32) || defined(__CYGWIN__)) && !defined(GRAPHENE_STATIC_COMPILATION)
+#  define _GRAPHENE_EXPORT __declspec(dllexport)
+#  define _GRAPHENE_IMPORT __declspec(dllimport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#  define _GRAPHENE_EXPORT __attribute__((visibility("default")))
+#  define _GRAPHENE_IMPORT
+#else
+#  define _GRAPHENE_EXPORT
+#  define _GRAPHENE_IMPORT
+#endif
+
+#ifdef GRAPHENE_COMPILATION
+#  define _GRAPHENE_API _GRAPHENE_EXPORT
+#else
+#  define _GRAPHENE_API _GRAPHENE_IMPORT
+#endif
+
+#define _GRAPHENE_PUBLIC _GRAPHENE_API extern
+
 /**
  * GRAPHENE_ENCODE_VERSION:
  * @major: a major version
